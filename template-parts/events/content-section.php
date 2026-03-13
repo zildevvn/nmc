bec68ef2fb06fad4f540e09494c70c733c8b515e
zildevvn <vuongdevvn0509@gmail.com>
2026-03-13 17:01:18 +0700

    add event page

---
+++ template-parts/events/content-section.php
@@ -0,0 +1,7 @@
+<section class="nmc-section content-section">
+    <div class="container">
+        <div class="content-section__content text-center">
+            <iframe src="https://app.9mbc.co.jp/embed/events/calendar"></iframe>
+        </div>
+    </div>
+</section>
