d635954c5c3a40eb6aa56993438f0248dc5a4b74
zildevvn <vuongdevvn0509@gmail.com>
2026-03-30 23:47:37 +0700

    update container and hide voice section

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 	<?php if(have_posts()) : ?><?php while(have_posts()) : the_post(); ?>
 		<main class="site-main template-home">
-			<div class="container" style="min-height:50vh; padding-top:100px"> 
+			<div class="container" style="min-height:50vh; padding-top:100px;  max-width: 1000px;"> 
 				<?php the_content() ?>
 			</div>
 		</main>
